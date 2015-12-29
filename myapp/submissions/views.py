4d67c1b533d22c7be6d214ef20440268cf82978a
Junhua <junhua.viva@gmail.com>
2015-12-29 23:40:54 +0800

    custom submission function

--- myapp/submissions/views.py
+++ myapp/submissions/views.py
@@ -45,53 +45,64 @@ class CodeSubmissionViewSet(DefaultsMixin, viewsets.ModelViewSet):
     serializer_class = CodeSubmissionSerializer
     filter_fields = ['question']
 
-    # def create(self, request):
-    #     """
-    #     Override default POST method:
-    #     1. create Submission
-    #     2. create Performance report
+    def create(self, request):
+        """
+        Override default POST method:
+        1. create Submission
+        2. create Performance report
 
-    #     """
+        """
 
        
-    #     data = request.data
-    #     serializer = CodeSubmissionSerializer(data=data)
-    #     if serializer.is_valid():
-    #         try:
-    #             user = request.user
-    #             code=data.get('code', None)
-    #             question=Question.objects.get(id=data.get('question',None))
+        data = request.data
+        serializer = CodeSubmissionSerializer(data=data)
 
-    #             print "************************"
-    #             print user
-    #             print code
-    #             print question
-    #         except:
-    #             return Response({"message": "error"}, status=400)
+        if serializer.is_valid():
+            try:
+                user = request.user
+                code=data.get('code', None)
+                question=Question.objects.get(id=data.get('question',None))
 
-    #         subm = CodeSubmission(
-    #             created_by=user,
-    #             code=code,
-    #             question=question
-    #         )
+            except:
+                return Response({"message": "Required user, code and question params"}, status=400)
+
+            # To be edited 
+            complexity = -1
+            memory = -1
+            time = -1
+            correctness = -1
+
             
+            report = PerformanceReport(
+                complexity=complexity,
+                memory=memory,
+                time=time,
+                correctness=correctness
+            )
+            try:
+                report.save()
+            except:
+                return Response({"message": "Failed to create report"}, status=400)
+    
+            subm = CodeSubmission(
+                created_by=user,
+                code=code,
+                question=question,
+                performance_report=report
+            )
 
-    #         # complexity = -1
-    #         # memory = -1
-    #         # time = -1
-    #         # correctness = -1
+            try:    
+                subm.save()
+            except:
+                return Response(
+                    {"message": "Failed to create submission"}, 
+                    status=400
+                )
 
-    #         # report = PerformanceReport(
-    #         #     complexity=complexity,
-    #         #     memory=memory,
-    #         #     time=time,
-    #         #     correctness=correctness
-    #         # )
-    #         # subm.performance_report = report
-    #         subm.save()
-    #         return Response({"result", subm}, status=200)
 
-    #     return Response(status.HTTP_400_BAD_REQUEST)
+            return Response({"result": "submission complete"}, status=200)
+
+        return Response({"message": "error"}, status=400)
 
 
 class BlanksSubmissionViewSet(DefaultsMixin, viewsets.ModelViewSet):
