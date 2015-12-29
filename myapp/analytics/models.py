0a281f8fdab78e32180490dd1f275a5b256c6eae
Junhua <junhua.viva@gmail.com>
2015-12-29 10:19:52 +0800

    added analytics model, change unique_together for asseesment

--- myapp/analytics/models.py
+++ myapp/analytics/models.py
@@ -3,6 +3,11 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.db import models
 
+import timeit
+import numpy as np
+import sys
+import StringIO
+
 
 PEER_READABLE_RANK = (
     (0, _("Unreadable")),
@@ -19,6 +24,50 @@ PEER_SMART_RANK = (
     (3, _("Brilliant")),
     (4, _("Genius"))
 )
+
+
+class PerormanceReportManger(models.Manager):
+
+    def time_exec(self, code, times=10, lang="Python"):
+        """
+        Measure the execution time for a snippet in microsecond. 
+        Taking average of 10 execution times by default.
+        """
+        try:
+            record = np.mean(timeit.repeat(code, repeat=times))
+            return record
+        except:
+            # Return -1 if there's an error
+            return -1
+
+    def run(self, code, lang="Python"):
+        """
+        Execute code input.
+        Return (
+            str: output, 
+            bool: successful
+            )
+        """
+
+        codeOut = StringIO.StringIO()
+        codeErr = StringIO.StringIO()
+        sys.stdout = codeOut
+        sys.stderr = codeErr
+
+        exec code
+
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+        result = codeOut.getvalue() or codeErr.getvalue()
+
+        if codeOut.getvalue():
+            return result, True
+
+        return result, False
+
+    
+
 
 
 class PerformanceReport(models.Model):
@@ -75,13 +124,14 @@ class PerformanceReport(models.Model):
         verbose_name_plural = _('performance_reports')
         ordering = ['date_created']
 
+
 class PeerRankReport(models.Model):
-    
+
     date_created = models.DateTimeField(
         _('date created'),
         default=timezone.now
     )
-    
+
     def __str__(self):
         return str(self.id)
 
@@ -89,6 +139,7 @@ class PeerRankReport(models.Model):
         verbose_name = _('peer_rank_report')
         verbose_name_plural = _('peer_rank_reports')
         ordering = ['date_created']
+
 
 class PeerRank(models.Model):
 
