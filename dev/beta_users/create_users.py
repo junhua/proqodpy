b61782c580b99895e748607703cfa97b8b4d4276
Junhua <junhua.viva@gmail.com>
2016-02-04 15:50:30 +0800

    updated create user

---
+++ dev/beta_users/create_users.py
@@ -0,0 +1,22 @@
+import requests
+import csv
+url = 'http://localhost:8000/v1/auth/register/'
+# data = {
+#     "sid" : "00001",
+#     "email" : "00001@test.proqod.com",
+#     "password" : "123",
+#     'user_type' : '0',
+#     'is_admin' : '0',
+# }
+
+with open('beta_users.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for [sid, user_type, is_admin, password] in spamreader:
+        data = {
+            "sid": sid,
+            "email": "%s@test.proqod.com" % sid,
+            "password": password,
+            'user_type': user_type,
+            'is_admin': is_admin
+        }
+        response = requests.post(url, data=data)
