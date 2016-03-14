13d5a452392af1be57967b09ab1deca342aeab90
yaojie <limyaojie93@gmail.com>
2016-03-14 11:18:12 +0800

    updated authentication method to use jwt authentication for new ui

--- proqodpy/settings.py
+++ proqodpy/settings.py
@@ -86,6 +86,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'rest_framework.filters.DjangoFilterBackend',
