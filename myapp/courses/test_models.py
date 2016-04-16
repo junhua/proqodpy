86a467f2e9dc094a5a55c413e823cec793d1d0b5
Junhua <junhua.viva@gmail.com>
2016-04-16 22:16:19 +0800

    updated tests

--- myapp/courses/test_models.py
+++ myapp/courses/test_models.py
@@ -15,15 +15,15 @@ class UnittestTestCase(TestCase):
         )
 
         ProgrammingQuestion.objects.create(
-            assessment=Assessment.objects.all()[0],
+            assessment=Assessment.objects.first(),
             number=1,
             type=0,
             description="",
             solution="",
             default_code="",
             code_signature="plus",
-
         )
+
         UnitTest.objects.create(
             visibility=0,
             type=0,
@@ -31,23 +31,21 @@ class UnittestTestCase(TestCase):
             test_content='',
             inputs=[1, 2],
             expected_output=3,
-            question=ProgrammingQuestion.objects.all()[0],
+            question=ProgrammingQuestion.objects.first(),
         )
+        self.code = "def plus(a,b): return a+b"
 
     def test_unittest_can_run(self):
         """ unit test can run """
-        unittest = UnitTest.objects.all()[0]
-        code = "def plus(a,b): return a+b"
+        unittest = UnitTest.objects.first()
 
-        result = unittest.run(code)
+        result = unittest.run(self.code)
         self.assertEqual(type(result), dict)
 
     def test_unittest_can_execute(self):
         """ unit test can execute correctly """
-        unittest = UnitTest.objects.all()[0]
-        code = "def plus(a,b): return a+b"
+        unittest = UnitTest.objects.first()
 
-        result = unittest.execute(code)
+        result = unittest.execute(self.code)
 
         self.assertNotEqual(result, None)
-        # self.assertEqual(result[0], unittest.expected_output)
